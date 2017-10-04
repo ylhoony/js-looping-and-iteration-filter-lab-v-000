eb1e896b5bd8410a5ab729eecd2915ce1fda952b
Hoon Lee <yl.hoony@gmail.com>
2017-10-04 16:17:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // Code your solution in this file
  
+function findMatching(collection, name) {
+  const sameName = [];
+
+  for (const nameEach of collection) {
+    if (nameEach === name) {
+      sameName.push(nameEach);
+    }
+  }
+  
+}
