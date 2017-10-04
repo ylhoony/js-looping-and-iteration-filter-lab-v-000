fbc7d71335ea51e784bbfe7a7b83ac99c3040d5c
Hoon Lee <yl.hoony@gmail.com>
2017-10-04 16:43:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 // Code your solution in this file
 
-// function findMatching(collection, name) {
-//   const sameName = [];
-//
-//   for (const nameEach of collection) {
-//     if (nameEach.toUpperCase() === name.toUpperCase()) {
-//       sameName.push(nameEach);
-//     }
-//   }
-//   return sameName;
-// }
-
 function findMatching(collection, name) {
   return collection.filter(function(driver) {
     return driver.toUpperCase() === name.toUpperCase();
